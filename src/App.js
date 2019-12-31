fe9b1a6df1002e4a527e21f723c5628f48a2ebbb
tom <gaoqiang@hua-cloud.com.cn>
2019-12-31 09:38:44 +0800

    feat: 添加动态修改播放数据的逻辑

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./App.css";
 
 import AutoPlayTab from "./AutoPlayTab";
 
-function App() {
-  return (
-    <div className="App">
-      <AutoPlayTab />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tabList: []
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        tabList: ["0", "1", "2", "3"]
+      });
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <div>
+        <AutoPlayTab tabList={this.state.tabList} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/AutoPlayTab.js
+++ src/AutoPlayTab.js
@@ -4,17 +4,27 @@ export default class AutoPlayTab extends Component {
   constructor(props) {
     super(props);
 
+    this.play = this.play.bind(this);
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
     this.handleMouseLeave = this.handleMouseLeave.bind(this);
 
+    this.source = [];
+
     this.state = {
       tabIndex: 0,
-      tabList: [0, 1, 2]
+      tabList: []
     };
   }
 
   componentDidMount() {
     this.play();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.tabList !== this.props.tabList) {
+      console.log("componentDidUpdate");
+      this.source = [...this.props.tabList];
+    }
   }
 
   handleMouseEnter() {
@@ -26,42 +36,66 @@ export default class AutoPlayTab extends Component {
   }
 
   play() {
-    if (this.state.tabIndex >= this.state.tabList.length) {
-      this.reset();
-    }
-
-    this.timer = setTimeout(() => {
+    const intervalFunc = () => {
       this.doSomething();
-      this.setState({ tabIndex: this.state.tabIndex + 1 }, this.play);
-    }, 1000);
+
+      this.timer = setTimeout(intervalFunc, 1000);
+    };
+
+    this.timer = setTimeout(intervalFunc, 1000);
   }
 
   doSomething() {
-    console.log("doSomething");
+    const index = this.state.tabList.pop();
+    if (index) {
+      console.log(index);
+    } else {
+      console.log("reset");
+      this.reset();
+    }
   }
 
   reset() {
-    this.setState({ tabIndex: 0 });
+    this.setState({
+      tabList: [...this.source]
+    });
   }
 
   render() {
-    const { tabIndex, tabList } = this.state;
-
-    return (
-      <ul>
-        {tabList.map(item => {
-          return (
-            <li
-              key={item}
-              onMouseEnter={this.handleMouseEnter}
-              onMouseLeave={this.handleMouseLeave}
-              className={tabIndex === item ? "active" : ""}
-            >
-              {item}
-            </li>
-          );
-        })}
-      </ul>
-    );
+    return null;
   }
 }
+
+// class AutoPlay {
+//     constructor() {
+//         this.timer = null;
+//         this.dataList = [];
+//         this.source = [...this.dataList];
+//     }
+
+//     play() {
+//         this.doSomething();
+
+//         this.timer = setTimeout(this.play, 1000);
+//     }
+
+//     stop() {
+//         clearTimeout(this.timer);
+//     }
+
+//     doSomething() {
+//         const item = this.dataList.pop();
+
+//         // 判断一轮数据的播放是否完成 完成则重置
+//         if (item) {
+//             this.dispatchAction(item);
+//         } else {
+//             // 重新来过
+//             this.reset();
+//         }
+//     }
+
+//     reset() {
+//         this.dataList = [...this.source];
+//     }
+// }
