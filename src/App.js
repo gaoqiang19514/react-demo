228da6f12b6ea752c7359306d820cd726cda64ba
tom <gaoqiang@hua-cloud.com.cn>
2019-12-31 16:43:34 +0800

    feat: 添加基础示例

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route } from "react-router-dom";
+
+import "./App.css";
+
+import history from "./services/history";
+
+// BrowserRouter和Router基本上是一样的，只是Router可以在外部传入history，BrowserRouter则是内置了history
+
+function Home() {
+  return (
+    <div>
+      Home
+      <button
+        onClick={() => {
+          history.push("/about");
+        }}
+      >
+        to /about
+      </button>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      About
+      <button
+        onClick={() => {
+          history.push("/home");
+        }}
+      >
+        to /home
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Route path="/" exact component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+      </Router>
     </div>
   );
 }
