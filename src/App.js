820e7d088c7732fa7bdf4146033dc450ad17c9ae
tom <gaoqiang19514@163.com>
2020-01-08 22:33:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { createForm } from "rc-form";
+
+import "antd-mobile/dist/antd-mobile.css";
+
+class Example extends Component {
+  submit = () => {
+    this.props.form.validateFields((error, value) => {
+      if (!error) {
+        // console.log("ok", value);
+      } else {
+        // console.log("error", error);
+      }
+    });
+
+    console.log(this.props.form.getFieldsValue(['name', 'age']))
+  };
+
+  render() {
+    let errors;
+    const { getFieldProps, getFieldError } = this.props.form;
+
+    return (
+      <div>
+        <input
+          {...getFieldProps("name", {
+            initialValue: "",
+            rules: [{ required: true }]
+          })}
+        />
+        <input
+          {...getFieldProps("age", {
+            initialValue: "",
+            rules: [{ required: true }]
+          })}
+        />
+
+        {(errors = getFieldError("required") ? errors.join(",") : null)}
+        <button onClick={this.submit}>submit</button>
+      </div>
+    );
+  }
+}
+
+const ExampleWrapper = createForm()(Example);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExampleWrapper />
     </div>
   );
 }
