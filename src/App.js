051ce033622e17ec1716fe95a2aa61e282400e55
tom <gaoqiang19514@163.com>
2020-01-11 17:35:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import searchHOC from "./SearchWrapper";
 const UserWrapper = userHOC(User);
 
 const SearchWrapper = searchHOC(Search);
+const SearchWrapper2 = searchHOC(Search);
 
 function App() {
   return (
     <div className="App">
       <UserWrapper />
       <SearchWrapper />
+      <SearchWrapper2 />
     </div>
   );
 }
