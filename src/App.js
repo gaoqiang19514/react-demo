b76532b88609538835f700fdb771cc8acd7b11df
gaoqiang19514 <gaoqiang19514@163.com>
2020-01-30 23:20:21 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [num, change] = useState(0);
+  const [show, changeShow] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{num}</h1>
+      {show && <div>hello</div>}
+
+      <button onClick={() => change(num + 1)}>Click me</button>
+      <button onClick={() => changeShow(!show)}>change show</button>
     </div>
   );
 }
