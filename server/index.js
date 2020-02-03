dcb3bf427200f04058ca531b134daac653d344d2
gaoqiang19514 <gaoqiang19514@163.com>
2020-02-03 18:36:44 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/static", express.static("./server/public"));
+
+app.use((req, res, next) => {
+  const { token } = req.query;
+  if (token) {
+    return next();
+  }
+  res.send("缺少token");
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("middleware");
+    next();
+  },
+  (req, res) => {
+    res.send(`<h1>Welcome Home</h1>token: ${req.query.token}`);
+  }
+);
+
+app.listen(3000, () => {
+  console.log("server running port on 3000");
+});
