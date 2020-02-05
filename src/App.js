40013e966e49af07f3045d18b8cb040085ada120
gaoqiang19514 <gaoqiang19514@163.com>
2020-02-05 13:41:36 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Map from "./Map";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{ width: "100%", height: "100%" }}>
+        <Map />
+      </div>
+    );
+  }
 }
-
-export default App;
