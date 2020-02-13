111d9fa5b635b35c7d27d7d94029b4a6f6a646d4
高强 <gaoqiang@hua-cloud.com.cn>
2020-02-13 18:48:19 +0800

    add demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,125 @@
 import React, { Component } from "react";
 
 import "./App.css";
-import Map from "./Map";
+import Example from "./Example";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.mapLoad = this.mapLoad.bind(this);
+  }
+
+  // 自定义数据源
+  addSources(map) {
+    const center = map.getCenter();
+    const jsonData = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [center.lng + 0.03, center.lat + 0.02]
+          },
+          properties: {
+            title: "大学",
+            kind: "school"
+          }
+        },
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [center.lng + 0.01, center.lat - 0.01]
+          },
+          properties: {
+            title: "公园",
+            kind: "park"
+          }
+        },
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [center.lng - 0.03, center.lat - 0.02]
+          },
+          properties: {
+            title: "医院",
+            kind: "hospital"
+          }
+        }
+      ]
+    };
+
+    map.addSource("pointSource", {
+      type: "geojson",
+      data: jsonData
+    });
+  }
+
+  // 自定义图层
+  addLayers(map) {
+    map.addLayer({
+      id: "circleLayer",
+      type: "circle",
+      source: "pointSource",
+      layout: {
+        visibility: "visible"
+      },
+      paint: {
+        "circle-radius": 10,
+        "circle-color": {
+          type: "categorical",
+          property: "kind",
+          stops: [
+            ["school", "#ff0000"],
+            ["park", "#00ff00"],
+            ["hospital", "#0000ff"]
+          ],
+          default: "#ff0000"
+        },
+        "circle-opacity": 0.8
+      },
+      minzoom: 7,
+      maxzoom: 17.5
+    });
+  }
+
+  mapLoad(map) {
+    // 好像没什么区别？
+    this.addSources(map);
+    this.addLayers(map);
+
+    setTimeout(() => {
+      map.easeTo({
+        center: [116.46, 39.92],
+        zoom: 8,
+        bearing: 0,
+        pitch: 60,
+        duration: 2000
+      });
+
+      setTimeout(() => {
+        // 上移100像素
+        map.panBy([0, 100]);
+      }, 1200);
+    }, 1000);
+  }
+
   render() {
     return (
       <div style={{ width: "100%", height: "100%" }}>
-        <Map />
+        <Example
+          options={{
+            center: [116.46, 39.92],
+            zoom: 10,
+            pitch: 0,
+            maxZoom: 17,
+            minZoom: 3,
+            loadCallback: this.mapLoad
+          }}
+        />
       </div>
     );
   }
---
+++ src/Example/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+const mapStyle = {
+  width: "100%",
+  height: "100%"
+};
+
+class Map extends Component {
+  componentDidMount() {
+    this.setupMap();
+    this.map = this.createMap(this.props.options);
+
+    this.map.repaint = true;
+
+    if (this.map) {
+      this.map.on("load", this.load.bind(this));
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.map) {
+      this.map.remove();
+      this.map = null;
+    }
+  }
+
+  setupMap() {
+    /**
+     * 全局参数设置
+     */
+    window.minemap.domainUrl = "//minedata.cn";
+    window.minemap.dataDomainUrl = "//datahive.minedata.cn";
+    window.minemap.spriteUrl = "//minedata.cn/minemapapi/v2.0.0/sprite/sprite";
+    window.minemap.serviceUrl = "//minedata.cn/service/";
+
+    /**
+     * token、solution设置
+     */
+    window.minemap.accessToken = "25cc55a69ea7422182d00d6b7c0ffa93";
+    window.minemap.solution = 2365;
+  }
+
+  createMap(options = {}) {
+    /**
+     * 初始化地图实例
+     */
+    return new window.minemap.Map({
+      container: this.mapRef,
+      style: "//minedata.cn/service/solu/style/id/2374",
+      center: [103.602093, 36.10469],
+      zoom: 13,
+      pitch: 0,
+      maxZoom: 17,
+      minZoom: 3,
+      ...options
+    });
+  }
+
+  load() {
+    const { loadCallback = () => {} } = this.props.options;
+
+    loadCallback(this.map);
+  }
+
+  render() {
+    return <div style={mapStyle} ref={ref => (this.mapRef = ref)}></div>;
+  }
+}
+
+export default Map;
