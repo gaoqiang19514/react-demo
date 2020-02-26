e2af036cca479b495d15464c474f8ea6264fa499
高强 <gaoqiang@hua-cloud.com.cn>
2020-02-27 00:54:28 +0800

    add inverse traverse tree

--- src/App.js
+++ src/App.js
@@ -2,6 +2,53 @@ import React from "react";
 
 import Example from "./Example";
 
+const treeData = [
+  {
+    id: 1,
+    name: "tom",
+    children: [
+      {
+        id: 2,
+		name: "lina",
+		num: 10,
+        children: [
+          {
+            id: 4,
+            name: "lily"
+          },
+          {
+            id: 5,
+            name: "lily"
+          }
+        ]
+      },
+      {
+        id: 3,
+        name: "zhou"
+      }
+    ]
+  }
+];
+
+// 从叶子开始向上遍历树
+function test(data) {
+  let arr = [];
+  data.forEach(item => {
+    const node = {
+      id: item.id,
+      name: item.name
+    };
+    if (item.children) {
+      arr.push(...test(item.children));
+    }
+    arr.push(node);
+  });
+  return arr;
+}
+
+const res = test(treeData);
+console.log(res);
+
 function App() {
   return (
     <div>
