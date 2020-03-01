fd768e714b1ef96e881f1a33a71294c76e546c12
高强 <gaoqiang@hua-cloud.com.cn>
2020-03-01 12:54:34 +0800

    update

---
+++ src/components/UseEffectExample.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, Component } from "react";
+
+export class Example extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    this.updateTitle(this.state.count);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      this.updateTitle(this.state.count);
+    }
+  }
+
+  updateTitle(count) {
+    document.title = `You clicked ${count} times`;
+  }
+
+  handleClick() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.handleClick}></button>
+      </div>
+    );
+  }
+}
+
+function updateTitle(count) {
+  document.title = `You clicked ${count} times`;
+}
+
+// useEffect是 componentDidMount, componentDidUpdate, 和 componentWillUnmount三个生命周期钩子的组合
+// - useEffect依赖用空数组就是componentDidMount
+// - useEffect返回函数就是componentWillUnmount
+// - useEffect加依赖就是componentDidUpdate
+function UseEffectExample() {
+  const [count, setCount] = useState(0);
+
+  // 跟在componentDidUpdate判断state变化后更新是一样的
+  useEffect(() => {
+    updateTitle(count);
+  }, [count]);
+
+  return (
+    <div>
+      <button type="button" onClick={() => setCount(count + 1)}></button>
+    </div>
+  );
+}
+
+export default UseEffectExample;
