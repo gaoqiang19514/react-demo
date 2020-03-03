e10d36844abfabac1ff2f1cbfbf76737be421968
高强 <gaoqiang@hua-cloud.com.cn>
2020-03-03 22:51:03 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Example from "./components/Example";
 import WithoutMemo from "./components/WithoutMemo";
 import UseMemoExample from "./components/UseMemoExample";
 
+function Demo(props) {
+  const [data, setData] = useState([...props.data]);
+
+  const handleClick = () => {
+    data.pop();
+    setData([...data]);
+  };
+
+  return (
+    <div>
+      <ul>
+        {data.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Dome</button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
+      <Demo data={[1, 2, 3]} />
       <UseMemoExample />
     </div>
   );
