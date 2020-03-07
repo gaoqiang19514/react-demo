ab3da4a24edff67615e17645edcf8a82c02bd9a3
高强 <gaoqiang@hua-cloud.com.cn>
2020-03-07 23:23:44 +0800

    feat: add FormExample

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import React from "react";
 import TreeExample from "./components/TreeExample";
 import ListViewExample from "./components/ListViewExample";
 import TableExample from "./components/TableExample";
+import FormExample from "./components/FormExample";
 
 function App() {
   return (
     <div>
+      <h2>FormExample</h2>
+      <FormExample />
+
       <h2>TableExample</h2>
       <TableExample />
 
