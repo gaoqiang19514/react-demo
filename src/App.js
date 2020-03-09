6433f09d5d54e9726ecc6da3a563f7bf6259931f
高强 <gaoqiang@hua-cloud.com.cn>
2020-03-09 14:12:14 +0800

    feat: add TreeSelectExample.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import TreeExample from "./components/TreeExample";
 import ListViewExample from "./components/ListViewExample";
 import TableExample from "./components/TableExample";
 import FormExample from "./components/FormExample";
+import TreeSelectExample from "./components/TreeSelectExample";
 
 function App() {
   return (
     <div>
+      <h2>TreeSelectExample</h2>
+      <TreeSelectExample />
+
       <h2>FormExample</h2>
       <FormExample />
 
