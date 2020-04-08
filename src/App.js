8d6ce84f5cdf8ff1d4d2c75823ece053284e72e4
高强 <gaoqiang@hua-cloud.com.cn>
2020-04-08 17:09:38 +0800

    feat: create raect radio example code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    activeKey: "heatMap",
+    list: [
+      { key: "heatMap", name: "热力图" },
+      { key: "marker", name: "点位图" },
+      { key: "airline", name: "航线图" },
+    ],
+  };
+
+  onChange = (key) => {
+    this.setState({ activeKey: key });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.list.map(({ key, name }) => (
+          <label key={key}>
+            <input
+              type="radio"
+              checked={this.state.activeKey === key}
+              onChange={() => this.onChange(key)}
+            />
+            <span>{name}</span>
+          </label>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default App;
