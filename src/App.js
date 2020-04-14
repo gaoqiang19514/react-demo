07a7c18063b6087b67bd9420dfe8a895b45e7e54
高强 <gaoqiang@hua-cloud.com.cn>
2020-04-14 17:56:48 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+function calc(start = 6, times = 12) {
+  const radius = 200;
+  const posList = [];
+  let len = times;
+
+  // 角度
+  const angle = 360 / times;
+  // 弧度
+  const radian = (angle * Math.PI) / 180;
+
+  while (len > 0) {
+    const i = --len;
+    const x = (start + i) % times;
+
+    posList.push({
+      left: Math.round(Math.sin(radian * x) * radius),
+      top: Math.round(Math.cos(radian * x) * radius),
+    });
+  }
+
+  return posList;
+}
 
 function App() {
+  const postList = calc(6, 12);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="box">
+          <ul>
+            {items.map((item, index) => (
+              <li
+                key={index}
+                style={{
+                  top: postList[index].top,
+                  left: postList[index].left,
+                }}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
