2828017783dc1f281d5ddf052a2fb7cd6d26b8c6
高强 <gaoqiang@hua-cloud.com.cn>
2020-04-16 11:53:43 +0800

    feat: 添加点位重叠点击回调获取数据示例

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Cluster from "./components/Cluster";
 import MapBorder from "./components/MapBorder";
+import MarkerOverlap from './components/MarkerOverlap'
 
 class App extends Component {
   render() {
     return (
       <div style={{ width: "100%", height: "100%" }}>
-        <Cluster />
+		<MarkerOverlap />
+        {/* <Cluster /> */}
         {/* <MapBorder /> */}
       </div>
     );
---
+++ src/components/MarkerOverlap/index.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+
+import Map from "../Map";
+
+class MarkerOverlap extends Component {
+  addSources = (map, sourceId) => {
+    const center = map.getCenter();
+
+    var jsonData = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [center.lng, center.lat],
+          },
+          properties: {
+            title: "大学",
+            kind: "school",
+          },
+        },
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [center.lng + 0.00001, center.lat + 0.00001],
+          },
+          properties: {
+            title: "公园",
+            kind: "park",
+          },
+        },
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [center.lng - 0.1, center.lat - 0.1],
+          },
+          properties: {
+            title: "医院",
+            kind: "hospital",
+          },
+        },
+      ],
+    };
+
+    map.addSource(sourceId, {
+      type: "geojson",
+      data: jsonData,
+    });
+  };
+
+  addLayers = (map, sourceId) => {
+    map.addLayer({
+      id: "lineLayer",
+      type: "circle",
+      source: sourceId,
+      layout: {
+        visibility: "visible",
+      },
+      paint: {
+        "circle-radius": 10,
+        "circle-color": {
+          type: "categorical",
+          property: "kind",
+          stops: [
+            ["school", "#ff0000"],
+            ["park", "#00ff00"],
+            ["hospital", "#0000ff"],
+          ],
+          default: "#ff0000",
+        },
+        "circle-opacity": 0.8,
+      },
+    });
+  };
+
+  handleMarkerClick = (e) => {
+    const features = this.map.queryRenderedFeatures(e.point, {
+      layers: ["lineLayer"],
+    });
+
+    console.log(features);
+  };
+
+  initEvents = (map) => {
+    map.on("click", this.handleMarkerClick);
+  };
+
+  loadCallback = (map) => {
+    this.map = map;
+
+    const sourceId = "sourceId";
+    this.addSources(map, sourceId);
+    this.addLayers(map, sourceId);
+    this.initEvents(map);
+  };
+
+  render() {
+    return (
+      <Map
+        options={{
+          center: [116.46, 39.92],
+          zoom: 10,
+          pitch: 0,
+          maxZoom: 17,
+          minZoom: 3,
+          loadCallback: this.loadCallback,
+        }}
+      />
+    );
+  }
+}
+
+export default MarkerOverlap;
