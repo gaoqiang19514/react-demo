3805f5b733f5af46567dc03d5a3f15f768a920e2
高强 <gaoqiang@hua-cloud.com.cn>
2020-04-29 15:21:15 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+
+// axios
+//   .post(
+//     "/zhzf/zfzh/zfzh-service/webapi/resourceMap/getResourceMapDevice",
+//     { areaCode: 4403 },
+//     {
+//       headers: {
+//         timestamp: "1588060061148",
+//         sign: "860a08b340273cef8f8a309aa1c5c654",
+//         nonce: "605962901588060000000",
+//         "X-AppCode": "zfzh-admin",
+//       },
+//     }
+//   )
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+// http://localhost.charlesproxy.com:3000/statistics-service/statistic/webapi/screen/highIncidenceTop
+// https://www.fastmock.site/mock/d0d715d7a91724b6f295ed88af4e7bda/mock/getPersonClockInData
+function main() {
+  axios
+    .post("http://localhost.charlesproxy.com:4000/getUsers")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={main}>start</button>
     </div>
   );
 }
---
+++ src/setupProxy.js
@@ -0,0 +1,27 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/zhzf/zfzh/",
+    createProxyMiddleware({
+      target: "http://szzhcg.com",
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    "/statistics-service",
+    createProxyMiddleware({
+      target: "http://gateway.zw-dev.com",
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    "/getUsers",
+    createProxyMiddleware({
+      target: "http://localhost:3000",
+      changeOrigin: true,
+    })
+  );
+};
