d999e219722d3a379c2346341facc4b2e2f12a65
高强 <gaoqiang@hua-cloud.com.cn>
2020-05-11 18:15:15 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import echarts from "echarts";
+import "echarts-wordcloud";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+function getColor(index) {
+  const colors = [
+    "#4293F4",
+    "#30DFBA ",
+    "#FFDB40",
+    "#FFA64F",
+    "#FF4B6D",
+    "#D1368C",
+    "#A179FF",
+  ];
+
+  return colors[index % colors.length];
 }
+export default class App extends Component {
+  componentDidMount() {
+    const chart = echarts.init(this.ref);
 
-export default App;
+    const data = [
+      { value: 5581, name: "多多" },
+      { value: 5019, name: "豆豆" },
+      { value: 3371, name: "Lucky" },
+      { value: 3280, name: "小白" },
+      { value: 3255, name: "嘟嘟" },
+      { value: 3011, name: "妞妞" },
+      { value: 2999, name: "乐乐" },
+      { value: 2634, name: "旺旺" },
+      { value: 2622, name: "可乐" },
+      { value: 2537, name: "球球" },
+    ];
+
+    data.map((item) => ({ ...item, value: Math.sqrt(item.value) }));
+
+    const option = {
+      series: [
+        {
+          type: "wordCloud",
+          sizeRange: [30, 80],
+          gridSize: 20,
+          rotationRange: [0, 45],
+          shape: "pentagon",
+          textStyle: {
+            normal: {
+              color: ({ dataIndex }) => {
+                console.log(1);
+                return getColor(dataIndex);
+              },
+            },
+          },
+          data: data.sort((a, b) => {
+            console.log(1);
+            return b.value - a.value;
+          }),
+        },
+      ],
+    };
+
+    chart.setOption(option);
+  }
+
+  render() {
+    return (
+      <div
+        className="main"
+        ref={(ref) => {
+          this.ref = ref;
+        }}
+      ></div>
+    );
+  }
+}
