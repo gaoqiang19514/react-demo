7482216a7526542b1e688b5fd596d131266f5b4b
高强 <gaoqiang@hua-cloud.com.cn>
2020-05-26 16:37:48 +0800

    chore: init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
 
 import "./App.css";
-import Cluster from "./components/Cluster";
-import MapBorder from "./components/MapBorder";
-import MarkerOverlap from './components/MarkerOverlap'
 
 class App extends Component {
   render() {
-    return (
-      <div style={{ width: "100%", height: "100%" }}>
-		<MarkerOverlap />
-        {/* <Cluster /> */}
-        {/* <MapBorder /> */}
-      </div>
-    );
+    return <div>
+		<div className="box"></div>
+	</div>;
   }
 }
 
