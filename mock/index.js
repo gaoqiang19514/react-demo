62482cb4c013f244ce8ed4d8b5f2164a3252b45f
tom <gaoqiang19514@163.com>
2020-06-13 16:05:59 +0800

    feat: 添加请求示例

--- mock/index.js
+++ mock/index.js
@@ -1,14 +1,12 @@
 export default {
   "GET /user": (req, res) => {
-    const interval = Math.random() * 10 * 1000;
-
     setTimeout(() => {
       res.send({
         code: 0,
         data: {
-          users: [{ id: 1, name: "tom", age: 30, interval }],
+          users: [{ id: 1, name: "tom", age: 30, key: req.query.key }],
         },
       });
-    }, interval);
+    }, 1000);
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import React from "react";
 import { from, fromEvent } from "rxjs";
-import { pluck, switchMap } from "rxjs/operators";
+import { pluck, map, debounceTime, switchMap, filter } from "rxjs/operators";
 import axios from "axios";
+
+function loadSuggestList(e) {
+  return axios.get("/user", {
+    params: {
+      key: e.target.innerHTML,
+    },
+  });
+}
+
+function SelectList() {
+  const [list, setList] = React.useState([
+    {
+      id: 1,
+      name: "tom",
+    },
+    {
+      id: 2,
+      name: "lina",
+    },
+  ]);
+  const ul = React.useRef();
+
+  React.useEffect(() => {
+    fromEvent(ul.current, "click")
+      .pipe(
+        filter((e) => e.target.matches("li")),
+        switchMap(loadSuggestList)
+      )
+      .subscribe((res) => {
+        console.log(res.data.data.users[0].key);
+      });
+  }, []);
+
+  return (
+    <>
+      <ul ref={ul}>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
   const [data, setData] = React.useState([]);
@@ -22,6 +65,7 @@ function App() {
 
   return (
     <div className="App">
+      <SelectList />
       <button ref={button} type="button">
         button
       </button>
