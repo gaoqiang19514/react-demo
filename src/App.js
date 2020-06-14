e9aa09cb86952ecda0f73be58c88b2dab6602c57
tom <gaoqiang19514@163.com>
2020-06-14 23:32:52 +0800

    chore: init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import uuid from "uuid";
+
+const API = {
+  getData: (key) => {
+    return new Promise((resolve) => {
+      console.log("request", key);
+
+      setTimeout(() => {
+        resolve({ name: "tom" });
+      }, 1000);
+    });
+  },
+};
 
 function App() {
+  const [data, setData] = React.useState({});
+  const [list, setList] = React.useState([
+    { key: "car", checked: true },
+    { key: "camera", checked: true },
+  ]);
+
+  const loadDataByKey = async (key) => {
+    const res = await API.getData(key);
+    setData(res);
+  };
+
+  React.useEffect(() => {
+    list.forEach((item) => {
+      // TODO: 怎么避免checked没有更新的项目发起请求呢？
+      if (item.checked) {
+        loadDataByKey(item.key);
+      }
+    });
+  }, [list]);
+
+  const handleChange = (key) => {
+    const nextList = list.map((item) => {
+      if (item.key === key) {
+        return {
+          ...item,
+          checked: !item.checked,
+        };
+      }
+      return {
+        ...item,
+      };
+    });
+
+    setList(nextList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((item) => (
+        <div key={item.key}>
+          <span>{item.key}</span>
+          <input
+            type="checkbox"
+            checked={item.checked}
+            onChange={() => handleChange(item.key)}
+          />
+        </div>
+      ))}
+      <div>data: {JSON.stringify(data)}</div>
     </div>
   );
 }
