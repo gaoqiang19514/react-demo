ae058ade2435f398c5295e12d291eca3b95c1660
tom <gaoqiang19514@163.com>
2020-06-30 00:00:28 +0800

    chore: init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Button, Input, Icon, Spin, Select } from "antd";
+import { debounce } from "lodash";
+import SearchInput from "./SearchInput";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+
+const { Option } = Select;
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some((field) => fieldsError[field]);
+}
+
+class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.lastFetchId = 0;
+    this.fetchUser = debounce(this.fetchUser, 800);
+
+    this.state = {
+      data: [],
+      value: [],
+      fetching: false,
+    };
+  }
+
+  componentDidMount() {
+    // To disable submit button at the beginning.
+    // this.props.form.validateFields();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  fetchUser = (value) => {
+    console.log("fetching user", value);
+    this.lastFetchId += 1;
+    const fetchId = this.lastFetchId;
+    this.setState({ data: [], fetching: true });
+    fetch("https://randomuser.me/api/?results=5")
+      .then((response) => response.json())
+      .then((body) => {
+        if (fetchId !== this.lastFetchId) {
+          // for fetch callback order
+          return;
+        }
+        const data = body.results.map((user) => ({
+          text: `${user.name.first} ${user.name.last}`,
+          value: user.login.username,
+        }));
+        this.setState({ data, fetching: false });
+      });
+  };
+
+  handleChange = (value) => {
+    this.setState({
+      value,
+      data: [],
+      fetching: false,
+    });
+  };
+
+  render() {
+    const {
+      getFieldDecorator,
+      getFieldsError,
+      getFieldError,
+      isFieldTouched,
+    } = this.props.form;
+    const { fetching, data, value } = this.state;
+
+    // Only show error after a field is touched.
+    const usernameError =
+      isFieldTouched("username") && getFieldError("username");
+    const passwordError =
+      isFieldTouched("password") && getFieldError("password");
+
+    return (
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <Form.Item
+          validateStatus={usernameError ? "error" : ""}
+          help={usernameError || ""}
+        >
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username!" }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item
+          validateStatus={passwordError ? "error" : ""}
+          help={passwordError || ""}
+        >
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }],
+          })(
+            <Select
+              mode="multiple"
+              labelInValue
+              placeholder="Select users"
+              notFoundContent={fetching ? <Spin size="small" /> : null}
+              filterOption={false}
+              onSearch={this.fetchUser}
+              onChange={this.handleChange}
+              style={{ width: 200 }}
+            >
+              {data.map((d) => (
+                <Option key={d.value}>{d.text}</Option>
+              ))}
+            </Select>
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={hasErrors(getFieldsError())}
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const WrappedDemo = Form.create({ name: "Demo" })(Demo);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <SearchInput placeholder="input search text" /> */}
+      <WrappedDemo />
     </div>
   );
 }
