a91725a093a587fc78b0e6ba128d21dca5ea1013
tom <gaoqiang19514@163.com>
2020-07-03 00:36:58 +0800

    feat: 添加非受控上传功能

--- src/components/UploadDemo.js
+++ src/components/UploadDemo.js
@@ -1,129 +1,65 @@
 import React from "react";
-import { Upload, Icon, Modal } from "antd";
-
-function getBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
-}
-
-/**
- * 下载后端返回的文件流
- * @param {String} url
- * @param {String} key
- * @param {String} value
- */
-export function download(url, paramList = []) {
-  const formElement = document.createElement("form");
-
-  formElement.method = "GET";
-  formElement.target = "_blank";
-  formElement.action = url;
-  formElement.style.display = "display:none;";
-
-  Object.keys(paramList).forEach((key) => {
-    const inputElement = document.createElement("input");
-    inputElement.name = key;
-    inputElement.value = paramList[key];
-    formElement.appendChild(inputElement);
-  });
-
-  document.body.appendChild(formElement);
-  formElement.submit();
-  document.body.removeChild(formElement);
-}
+import { Form, Button, Upload, Icon } from "antd";
 
 class UploadDemo extends React.Component {
-  state = {
-    previewVisible: false,
-    previewImage: "",
-    fileList: [
-      {
-        uid: "-1",
-        name: "image.png",
-        status: "done",
-        url:
-          "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-      },
-      {
-        uid: "-2",
-        name: "image.png",
-        status: "done",
-        url:
-          "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-      },
-      {
-        uid: "-3",
-        name: "image.png",
-        status: "done",
-        url:
-          "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-      },
-      {
-        uid: "-4",
-        name: "image.png",
-        status: "done",
-        url:
-          "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-      },
-      {
-        uid: "-5",
-        name: "image.png",
-        status: "error",
-      },
-    ],
-  };
-
-  handleCancel = () => this.setState({ previewVisible: false });
-
-  handlePreview = async (file) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
-    }
-
-    this.setState({
-      previewImage: file.url || file.preview,
-      previewVisible: true,
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
     });
   };
 
-  handleChange = ({ fileList }) => this.setState({ fileList });
-
-  handleDownClick = (file) => {
-    console.log(file);
-    download(file.response.url);
+  normFile = (e) => {
+    console.log("Upload event:", e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
   };
 
   render() {
-    const { previewVisible, previewImage, fileList } = this.state;
-    const uploadButton = (
-      <div>
-        <Icon type="plus" />
-        <div className="ant-upload-text">Upload</div>
-      </div>
-    );
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 },
+    };
 
     return (
-      <div className="clearfix">
-        <Upload
-          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-          listType="picture-card"
-          fileList={fileList}
-          onPreview={this.handlePreview}
-          onChange={this.handleChange}
-          showUploadList={{ showDownloadIcon: true }}
-          onDownload={this.handleDownClick}
-        >
-          {fileList.length >= 8 ? null : uploadButton}
-        </Upload>
-        <Modal visible={previewVisible} onCancel={this.handleCancel}>
-          <img alt="example" style={{ width: "100%" }} src={previewImage} />
-        </Modal>
-      </div>
+      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+        <Form.Item label="Dragger">
+          {getFieldDecorator("dragger", {
+            rules: [{ required: true, message: "Please select your country!" }],
+            valuePropName: "fileList",
+            getValueFromEvent: this.normFile,
+          })(
+            <Upload.Dragger
+              name="files"
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            >
+              <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload.
+              </p>
+            </Upload.Dragger>
+          )}
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
-export default UploadDemo;
+
+const WrappedUploadDemo = Form.create({ name: "upload_demo" })(UploadDemo);
+
+export default WrappedUploadDemo;
