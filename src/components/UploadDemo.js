366819de7782602e87291c17eebe96287c1cd2c1
tom <gaoqiang19514@163.com>
2020-07-04 11:38:51 +0800

    refactor: 优化代码

--- src/components/UploadDemo.js
+++ src/components/UploadDemo.js
@@ -1,7 +1,45 @@
 import React from "react";
-import { Form, Button, Upload, Icon } from "antd";
+import { Form, Modal, Upload, Button, Icon } from "antd";
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
 
 class UploadDemo extends React.Component {
+  state = {
+    previewVisible: false,
+    previewImage: "",
+  };
+
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.props.form.setFieldsValue({
+    //     fileList: [
+    //       {
+    //         uid: "-1",
+    //         name: "image.png",
+    //         status: "done",
+    //         url:
+    //           "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    //       },
+    //       {
+    //         uid: "-2",
+    //         name: "文件.rar",
+    //         status: "done",
+    //         className: "rtest",
+    //         url:
+    //           "http://gxiami.alicdn.com/xiami-desktop/update/%E8%99%BE%E7%B1%B3%E9%9F%B3%E4%B9%90-7.2.7-x64-0919.exe",
+    //       },
+    //     ],
+    //   });
+    // }, 1000);
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -21,52 +59,73 @@ class UploadDemo extends React.Component {
 
   render() {
     const { getFieldDecorator } = this.props.form;
-    const formItemLayout = {
-      labelCol: { span: 6 },
-      wrapperCol: { span: 14 },
-    };
+    const { previewVisible, previewImage } = this.state;
 
     return (
-      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <Form.Item label="Dragger">
-          {getFieldDecorator("dragger", {
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Item>
+          {getFieldDecorator("fileList", {
             rules: [{ required: true, message: "Please select your country!" }],
             valuePropName: "fileList",
-            initialValue: [
-              {
-                uid: "-1",
-                name: "image.png",
-                status: "done",
-                url:
-                  "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-              },
-            ],
+            initialValue: [],
             getValueFromEvent: this.normFile,
           })(
-            <Upload.Dragger
-              name="files"
+            <Upload
+              listType="picture-card"
+              onPreview={this.onPreview}
+              onDownload={this.onDownload}
+              showUploadList={{ showDownloadIcon: true }}
               action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
             >
-              <p className="ant-upload-drag-icon">
-                <Icon type="inbox" />
-              </p>
-              <p className="ant-upload-text">
-                Click or drag file to this area to upload
-              </p>
-              <p className="ant-upload-hint">
-                Support for a single or bulk upload.
-              </p>
-            </Upload.Dragger>
+              {this.renderBtn()}
+            </Upload>
           )}
         </Form.Item>
-        <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
+
+        <Modal
+          visible={previewVisible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <img alt="example" style={{ width: "100%" }} src={previewImage} />
+        </Modal>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
       </Form>
     );
   }
+
+  renderBtn = () => {
+    const fields = this.props.form.getFieldsValue();
+
+    if (fields.fileList.length < 1) {
+      return (
+        <Button>
+          <Icon type="upload" /> Click to Upload
+        </Button>
+      );
+    }
+
+    return null;
+  };
+
+  handleCancel = () => this.setState({ previewVisible: false });
+
+  onPreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+
+    this.setState({
+      previewImage: file.url || file.preview,
+      previewVisible: true,
+    });
+  };
+
+  onDownload = (file) => {
+    console.log(file);
+  };
 }
 
 const WrappedUploadDemo = Form.create({ name: "upload_demo" })(UploadDemo);
