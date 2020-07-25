0b7a1e073e588d8a8793a1ecd6d99f20cfbc7c9d
tom <gaoqiang19514@163.com>
2020-07-25 11:00:34 +0800

    feat: add example

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ function Demo(props) {
   return <div>Demo</div>;
 }
 
+function Demo2(props) {
+  console.log(props);
+  return <div>Demo2</div>;
+}
+
 const WithDemo = With(Demo);
+const WithDemo2 = With(Demo2);
 
 function App() {
   return (
     <div className="App">
       <WithDemo age={30} />
+      <WithDemo2 age={31} />
     </div>
   );
 }
