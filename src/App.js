794e2602831bce2e508efa44869945c3a03f47a6
tom <gaoqiang19514@163.com>
2020-07-26 15:28:28 +0800

    feat: 封装api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+import downloadIcon from "./logo.svg";
 import Map from "./Map";
 
 const mapStyle = {
@@ -9,22 +10,115 @@ const mapStyle = {
 };
 
 class Example extends Component {
-  setRef = (ref) => {
-    this.mapRef = ref;
+  mapLoaded = (instance) => {
+    this.mapInstance = instance;
+
+    this.markerHandle = this.drawMarkers();
+    this.lineHandle = this.drawLine();
+    this.polygonHandle = this.drawPolygon();
+    this.labelHandle = this.drawLabel();
   };
 
-  mapLoaded = () => {
-    this.markerList = this.mapRef.addMarkers([[114.085947, 22.547]]);
+  drawMarkers = () => {
+    // 如果需要制定图标，则给Marker传入图标参数就行了
+    const myIcon = new window.BMapGL.Icon(
+      downloadIcon,
+      new window.BMapGL.Size(30, 30)
+    );
 
-    setTimeout(() => {
-      this.markerList.remove();
-    }, 3000);
+    return this.mapInstance.addMarkers([[114.085947, 22.547]], {
+      icon: myIcon,
+    });
   };
 
+  drawLine = () => {
+    return this.mapInstance.addPolyline(
+      [
+        [114.085947, 22.547],
+        [114.185947, 22.747],
+      ],
+      {
+        strokeColor: "blue",
+        strokeWeight: 2,
+        strokeOpacity: 0.5,
+      }
+    );
+  };
+
+  drawPolygon = () => {
+    return this.mapInstance.adPolygon(
+      [
+        [114.085947, 22.547],
+        [114.185947, 22.747],
+        [114.285947, 22.847],
+        [114.385947, 22.947],
+      ],
+      {
+        strokeColor: "blue",
+        strokeWeight: 2,
+        strokeOpacity: 0.5,
+      }
+    );
+  };
+
+  drawLabel = () => {
+    return this.mapInstance.addLabels([
+      {
+        longitude: 114.085947,
+        latitude: 22.547,
+        content: "我是label",
+      },
+      {
+        longitude: 114.185947,
+        latitude: 22.647,
+        content: "我是label2",
+      },
+    ]);
+  };
+
+  handleRemoveMarkersClick = () => {
+    if (this.markerHandle) {
+      this.markerHandle.remove();
+    }
+  };
+
+  handleRemoveLineClick = () => {
+    if (this.lineHandle) {
+      this.lineHandle.remove();
+    }
+  };
+
+  handleRemovePolygonClick = () => {
+    if (this.polygonHandle) {
+      this.polygonHandle.remove();
+    }
+  };
+
+
+
+  handleRemoveLabelsClick = () => {
+    if (this.labelHandle) {
+      this.labelHandle.remove();
+    }
+  };
   render() {
     return (
       <div style={mapStyle}>
-        <Map ref={this.setRef} mapLoaded={this.mapLoaded} />
+        <Map mapLoaded={this.mapLoaded} />
+        <div style={{ position: "absolute", zIndex: 10, top: 0 }}>
+          <button type="button" onClick={this.handleRemoveMarkersClick}>
+            remove markers
+          </button>
+          <button type="button" onClick={this.handleRemoveLineClick}>
+            remove line
+          </button>
+          <button type="button" onClick={this.handleRemovePolygonClick}>
+            remove Polygon
+          </button>
+          <button type="button" onClick={this.handleRemoveLabelsClick}>
+            remove labels
+          </button>
+        </div>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -42,11 +42,13 @@ class Map extends Component {
     });
 
     this.map.addEventListener("tilesloaded", () => {
-      this.props.mapLoaded(this.map);
+      this.props.mapLoaded(this);
     });
+
+    this.map.setTrafficOn();
   }
 
-  addMarkers(coordinateList) {
+  addMarkers(coordinateList, options = {}) {
     const { map } = this;
 
     const pointList = coordinateList.map(
@@ -54,7 +56,7 @@ class Map extends Component {
     );
 
     const markerList = pointList.map(
-      (point) => new window.BMapGL.Marker(point)
+      (point) => new window.BMapGL.Marker(point, options)
     );
 
     markerList.forEach((marker) => {
@@ -65,6 +67,76 @@ class Map extends Component {
       markerList,
       remove() {
         markerList.forEach((marker) => map.removeOverlay(marker));
+      },
+    };
+  }
+
+  addPolyline(coordinateList, options = {}) {
+    const { map } = this;
+
+    const pointList = coordinateList.map(
+      ([longitude, latitude]) => new window.BMapGL.Point(longitude, latitude)
+    );
+
+    const polyline = new window.BMapGL.Polyline(pointList, options);
+
+    map.addOverlay(polyline);
+
+    return {
+      polyline,
+      remove() {
+        map.removeOverlay(polyline);
+      },
+    };
+  }
+
+  adPolygon(coordinateList, options) {
+    const { map } = this;
+
+    const pointList = coordinateList.map(
+      ([longitude, latitude]) => new window.BMapGL.Point(longitude, latitude)
+    );
+
+    const polygon = new window.BMapGL.Polygon(pointList, options);
+    map.addOverlay(polygon);
+
+    return {
+      polygon,
+      remove() {
+        map.removeOverlay(polygon);
+      },
+    };
+  }
+
+  addLabel(longitude, latitude, content, options) {
+    const { map } = this;
+
+    const point = new window.BMapGL.Point(longitude, latitude);
+    const label = new window.BMapGL.Label(content, {
+      position: point,
+      ...options,
+    });
+
+    map.addOverlay(label);
+
+    return {
+      remove() {
+        map.removeOverlay(label);
+      },
+    };
+  }
+
+  addLabels(labelList) {
+    const labelHandleList = labelList.map((item) => {
+      return this.addLabel(item.longitude, item.latitude, item.content, {
+        offset: new window.BMapGL.Size(10, 20),
+      });
+    });
+
+    return {
+      labelHandleList,
+      remove() {
+        labelHandleList.forEach((label) => label.remove());
       },
     };
   }
