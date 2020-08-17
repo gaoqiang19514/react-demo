7d030f0454462c4e3b8830ec82a55b10a4e7cbf7
tom <gaoqiang19514@163.com>
2020-08-18 00:28:52 +0800

    feat: 添加可编辑的table

--- src/App.js
+++ src/App.js
@@ -6,20 +6,55 @@ import styled from "styled-components";
 // import TableExample from "./components/TableExample";
 // import FormExample from "./components/FormExample";
 // import TreeSelectExample from "./components/TreeSelectExample";
-import DatePickerExample from "./components/DatePickerExample";
-import RangePickerExample from "./components/RangePickerExample";
+// import DatePickerExample from "./components/DatePickerExample";
+// import RangePickerExample from "./components/RangePickerExample";
+import EditableTable from "./components/EditableTable";
 
 const PageContainer = styled.div`
   margin: 30px;
 `;
 
 function App() {
+  const columns = [
+    {
+      title: "name",
+      dataIndex: "name",
+      width: "30%",
+      editable: false,
+    },
+    {
+      title: "age",
+      dataIndex: "age",
+      editable: false,
+    },
+    {
+      title: "address",
+      dataIndex: "address",
+      editable: true,
+    },
+  ];
+
+  const dataSource = [
+    {
+      key: "0",
+      name: "Edward King 0",
+      age: "32",
+      address: "London, Park Lane no. 0",
+    },
+    {
+      key: "1",
+      name: "Edward King 1",
+      age: "32",
+      address: "London, Park Lane no. 1",
+    },
+  ];
   return (
     <PageContainer>
+      <EditableTable columns={columns} dataSource={dataSource} />
       {/* <h2>禁用日期</h2> */}
       {/* <DatePickerExample /> */}
-      <h2>禁用日期和时间</h2>
-      <RangePickerExample />
+      {/* <h2>禁用日期和时间</h2> */}
+      {/* <RangePickerExample /> */}
       {/* <h2>TreeSelectExample</h2>
       <TreeSelectExample />
       <h2>FormExample</h2>
