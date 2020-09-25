513afcf30e5c183c7236aa4febee26419e02cc62
tom <gaoqiang19514@163.com>
2020-09-25 22:45:56 +0800

    feat: 优化展示效果

---
+++ src/components/Display/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "序号",
+    dataIndex: "serial",
+  },
+  {
+    title: "名称",
+    dataIndex: "name",
+  },
+  {
+    title: "数量",
+    dataIndex: "count",
+  },
+];
+
+const propTypes = {
+  dataSource: PropTypes.array,
+};
+
+const defaultProps = {
+  dataSource: [],
+};
+
+function Display(props) {
+  const { dataSource } = props;
+
+  return <Table columns={columns} dataSource={dataSource} />;
+}
+
+Display.propTypes = propTypes;
+Display.defaultProps = defaultProps;
+
+export default Display;
--- src/components/ParseInput/index.js
+++ src/components/ParseInput/index.js
@@ -8,13 +8,50 @@ import {
   Textarea,
   TextareaBox,
 } from "./styled";
+import Display from "../Display";
 
 function ParseInput() {
   const ref = useRef(null);
-  const [input, setInput] = useState("");
+  const [dataSource, setDataSource] = useState("");
 
   const onInput = (e) => {
-    setInput(e.target.value);
+    setDataSource(parseString(e.target.value));
+  };
+
+  const parseString = (str) => {
+    const nameSpace = {};
+    const newestNameSpace = {};
+    const lines = str.split("\n");
+
+    const arr = lines.map((line) => line.split(/\s+/));
+
+    arr.forEach(([key, value]) => {
+      if (!key) {
+        return;
+      }
+
+      if (nameSpace[key]) {
+        nameSpace[key].push(value);
+      } else {
+        nameSpace[key] = [value];
+      }
+    });
+
+    Object.keys(nameSpace).forEach((key) => {
+      const temp = nameSpace[key].reduce(
+        (total, count) => total + Number(count),
+        0
+      );
+      newestNameSpace[key] = temp;
+    });
+
+    const result = Object.keys(newestNameSpace).map((key, index) => ({
+      serial: index + 1,
+      name: key,
+      count: newestNameSpace[key],
+    }));
+
+    return result;
   };
 
   return (
@@ -29,7 +66,9 @@ function ParseInput() {
           <Button type="primary">计算</Button>
         </TextRight>
       </PaddingHorizontalPrimary>
-      <PaddingPrimary>{input}</PaddingPrimary>
+      <PaddingPrimary>
+        <Display dataSource={dataSource} />
+      </PaddingPrimary>
     </div>
   );
 }
