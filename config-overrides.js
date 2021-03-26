17968ba3431009695b7652da01bb1108d24726f9
tom <gaoqiang19514@163.com>
2021-03-26 23:19:07 +0800

    chore: 添加热更新

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
-module.exports = (config, env) => {
-
-    config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname, 'src'),
+module.exports = (config) => {
+    return {
+        ...config,
+        alias: {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, 'src'),
+        },
     };
-
-    return config;
 };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import List from '@/components/List';
+
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
     return (
