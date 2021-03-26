7a852826b61bbc0cada44272a158314dcbfa5e10
tom <gaoqiang19514@163.com>
2021-03-26 22:21:29 +0800

    chore: 优化开发环境相关代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-    extends: ['eslint-config-react-zerod'],
+    extends: ['eslint-config-zerod'],
 };
--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,11 @@
 const path = require('path');
 
 module.exports = (config, env) => {
-    console.log('process', process);
 
     config.resolve.alias = {
         ...config.resolve.alias,
         '@': path.resolve(__dirname, 'src'),
     };
-
-    config.module.rules.push({});
 
     return config;
 };
