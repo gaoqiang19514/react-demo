57ff21ee9f2118edb85ddd6e8337754fd026addb
gaoqiang19514 <gaoqiang19514@163.com>
2021-05-16 19:56:26 +0800

    refactor: 更新示例

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 
 import './App.css';
 
-function createPosition(lng, lat, z = 0) {
-  return window.Cesium.Cartesian3.fromDegrees(lng, lat, z);
+function createPosition(lng, lat, height = 0) {
+  return window.Cesium.Cartesian3.fromDegrees(lng, lat, height);
 }
 
 function App() {
@@ -46,33 +46,26 @@ function App() {
     });
 
     // 添加图片点位
-    const entity = viewer.entities.add({
+    viewer.entities.add({
+      id: 1234,
       name: '标点',
-      position: Cesium.Cartesian3.fromDegrees(114.113702, 22.6208),
+      position: window.Cesium.Cartesian3.fromDegrees(114.113702, 22.6208),
       billboard: {
-        image: './acting.png',
-        horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
-        verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+        image: 'https://img-static.mihoyo.com/mainPage/ys-logo.png',
+        horizontalOrigin: window.Cesium.HorizontalOrigin.CENTER,
+        verticalOrigin: window.Cesium.VerticalOrigin.BOTTOM,
         scale: 1,
       },
     });
 
-    // 添加文字点位
-    viewer.entities.add({
-      position: createPosition(114.113702, 22.6208),
-      label: {
-        text: '<div>123</div>',
-        font: '14pt Source Han Sans CN', //字体样式
-        fillColor: window.Cesium.Color.BLACK, //字体颜色
-        backgroundColor: window.Cesium.Color.AQUA, //背景颜色
-        showBackground: true, //是否显示背景颜色
-        style: window.Cesium.LabelStyle.FILL, //label样式
-        outlineWidth: 2,
-        verticalOrigin: window.Cesium.VerticalOrigin.CENTER, //垂直位置
-        horizontalOrigin: window.Cesium.HorizontalOrigin.LEFT, //水平位置
-        pixelOffset: new window.Cesium.Cartesian2(10, 0), //偏移
-      },
-    });
+    // 事件绑定
+    viewer.screenSpaceEventHandler.setInputAction((e) => {
+      console.log('e', e);
+      const position = viewer.scene.pickPosition(e.position);
+      // 三维坐标转二维
+      const point = window.vusd.point.formatPositon(position);
+      console.log('point', point);
+    }, window.Cesium.ScreenSpaceEventType.LEFT_CLICK);
   }, []);
   return (
     <div className="App">
@@ -89,3 +82,32 @@ export default App;
 // billboard可以实现哪些工呢？
 
 // 什么对象可以实现可以传入html并且绑定点击事件的点位？
+
+// 添加图片点位
+// const entity = viewer.entities.add({
+//   name: '标点',
+//   position: window.Cesium.Cartesian3.fromDegrees(114.113702, 22.6208),
+//   billboard: {
+//     image: './acting.png',
+//     horizontalOrigin: window.Cesium.HorizontalOrigin.CENTER,
+//     verticalOrigin: window.Cesium.VerticalOrigin.BOTTOM,
+//     scale: 1,
+//   },
+// });
+
+// 添加文字点位
+// viewer.entities.add({
+//   position: createPosition(114.113702, 22.6208),
+//   label: {
+//     text: '<div>123</div>',
+//     font: '14pt Source Han Sans CN', //字体样式
+//     fillColor: window.Cesium.Color.BLACK, //字体颜色
+//     backgroundColor: window.Cesium.Color.AQUA, //背景颜色
+//     showBackground: true, //是否显示背景颜色
+//     style: window.Cesium.LabelStyle.FILL, //label样式
+//     outlineWidth: 2,
+//     verticalOrigin: window.Cesium.VerticalOrigin.CENTER, //垂直位置
+//     horizontalOrigin: window.Cesium.HorizontalOrigin.LEFT, //水平位置
+//     pixelOffset: new window.Cesium.Cartesian2(10, 0), //偏移
+//   },
+// });
