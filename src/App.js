9b65da44a1be3dd13825987d6c6972dd80ce60b1
高强 <gaoqiang@hua-cloud.com.cn>
2021-05-25 18:46:31 +0800

    refactor: 添加覆盖物示例

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import html2canvas from 'html2canvas';
 
 import './App.css';
 
@@ -45,27 +46,93 @@ function App() {
       destination: createPosition(114.113702, 22.6208, 100000),
     });
 
-    // 添加图片点位
+    // 添加文字点位
     viewer.entities.add({
-      id: 1234,
-      name: '标点',
-      position: window.Cesium.Cartesian3.fromDegrees(114.113702, 22.6208),
-      billboard: {
-        image: 'https://img-static.mihoyo.com/mainPage/ys-logo.png',
-        horizontalOrigin: window.Cesium.HorizontalOrigin.CENTER,
-        verticalOrigin: window.Cesium.VerticalOrigin.BOTTOM,
-        scale: 1,
+      position: createPosition(114.113701, 24.6208),
+      label: {
+        text: '<div>123</div>',
+        font: '14pt Source Han Sans CN',
+        fillColor: window.Cesium.Color.BLACK,
+        backgroundColor: window.Cesium.Color.AQUA,
+        showBackground: true,
+        style: window.Cesium.LabelStyle.FILL,
+        outlineWidth: 2,
+        verticalOrigin: window.Cesium.VerticalOrigin.CENTER,
+        horizontalOrigin: window.Cesium.HorizontalOrigin.LEFT,
+        pixelOffset: new window.Cesium.Cartesian2(10, 0),
       },
     });
 
+    function createMarker() {
+      const div = document.createElement('div');
+      div.id = 'marker';
+      div.className = 'marker';
+
+      return div;
+    }
+
+    // html2canvas(document.getElementById('marker')).then((canvas) => {
+    //   console.log('canvas', canvas);
+    //   // 添加图片点位
+    //   viewer.entities.add({
+    //     id: 1234,
+    //     name: '标点',
+    //     position: window.Cesium.Cartesian3.fromDegrees(114.113702, 22.6208),
+    //     billboard: {
+    //       image: 'https://img-static.mihoyo.com/mainPage/ys-logo.png',
+    //     },
+    //   });
+    // });
+
+    const popUp = (pick, position) => {
+      const cesiumContainer = document.getElementById('cesiumContainer');
+
+      const div = document.createElement('div');
+      div.className = 'marker';
+      div.style.top = `${position.y}px`;
+      div.style.left = `${position.x}px`;
+      div.innerHTML = 'text';
+
+      cesiumContainer.appendChild(div);
+    };
+
     // 事件绑定
     viewer.screenSpaceEventHandler.setInputAction((e) => {
-      console.log('e', e);
-      const position = viewer.scene.pickPosition(e.position);
-      // 三维坐标转二维
-      const point = window.vusd.point.formatPositon(position);
-      console.log('point', point);
+      const pick = viewer.scene.pick(e.position);
+
+      if (pick) {
+        popUp(pick, e.position);
+      }
     }, window.Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+    viewer.scene.postRender.addEventListener(() => {
+      // 这里需要拿到屏幕上存在点位的数据，然后实时更新他们的位置
+    });
+
+    // To geographically place an HTML element on top of the Cesium canvas, we use
+    // scene.cartesianToCanvasCoordinates to map a world position to canvas x and y values.
+    // This example places and img element, but any element will work.
+
+    const marker = createMarker();
+    document.body.appendChild(marker);
+
+    const markerElem = document.getElementById('marker');
+    const scratch = new window.Cesium.Cartesian2();
+    viewer.scene.preRender.addEventListener(() => {
+      const position = window.Cesium.Cartesian3.fromDegrees(
+        114.113702,
+        22.6208,
+      );
+      const canvasPosition = viewer.scene.cartesianToCanvasCoordinates(
+        position,
+        scratch,
+      );
+
+      if (window.Cesium.defined(canvasPosition)) {
+        markerElem.style.top = `${canvasPosition.y}px`;
+        markerElem.style.left = `${canvasPosition.x}px`;
+      }
+    });
   }, []);
   return (
     <div className="App">
